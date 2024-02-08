20297a99629c90dd7d39fef0e49288a9be72829b
l1senochek <lisenochek96@gmail.com>
2024-02-08 15:35:25 +0300

    feat: add tests for doStuffByInterval

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,6 +1,6 @@
 // Uncomment the code below and write your tests
 // import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
-import { doStuffByTimeout } from '.';
+import { doStuffByInterval, doStuffByTimeout } from '.';
 
 describe('doStuffByTimeout', () => {
   const timeout = 500;
@@ -34,6 +34,8 @@ describe('doStuffByTimeout', () => {
 });
 
 describe('doStuffByInterval', () => {
+  const interval = 500;
+
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -43,11 +45,23 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+
+    doStuffByInterval(callback, interval);
+    jest.advanceTimersByTime(interval);
+
+    expect(callback).toHaveBeenCalledWith();
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+
+    doStuffByInterval(callback, interval);
+    jest.advanceTimersByTime(interval);
+    jest.advanceTimersByTime(interval);
+    jest.advanceTimersByTime(interval);
+
+    expect(callback).toHaveBeenCalledTimes(3);
   });
 });
 
