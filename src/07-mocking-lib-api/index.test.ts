7abae00c14dd433e7b458ca02feaa89825ea6c80
l1senochek <lisenochek96@gmail.com>
2024-02-09 15:47:55 +0300

    feat: add test for throttledGetDataFromApi

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -4,15 +4,16 @@ import { THROTTLE_TIME, throttledGetDataFromApi } from './index';
 jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  const baseURL = 'https://jsonplaceholder.typicode.com';
+  const data = 'data';
+  const postsUrl = '/posts';
+
   afterEach(() => {
-    jest.clearAllMocks();
     jest.useRealTimers();
+    jest.clearAllMocks();
   });
 
   test('should create instance with provided base url', async () => {
-    const baseURL = 'https://jsonplaceholder.typicode.com';
-    const data = 'data';
-    const postsUrl = '/posts';
     const axiosInstanceMock = {
       get: jest.fn().mockResolvedValueOnce({ data: data }),
     } as unknown as AxiosInstance;
@@ -20,11 +21,11 @@ describe('throttledGetDataFromApi', () => {
       typeof axios.create
     >;
     axiosCreateMock.mockReturnValueOnce(axiosInstanceMock);
-
     jest.advanceTimersByTime(THROTTLE_TIME);
-    await new Promise((resolve) => process.nextTick(resolve));
+    await Promise.resolve();
 
     const result = await throttledGetDataFromApi(postsUrl);
+    jest.runAllTimers();
 
     expect(axios.create).toHaveBeenCalledWith({ baseURL });
     expect(axiosInstanceMock.get).toHaveBeenCalledWith(postsUrl);
@@ -36,6 +37,20 @@ describe('throttledGetDataFromApi', () => {
   });
 
   test('should return response data', async () => {
-    //
+    const data = 'data';
+    const postsUrl = '/posts';
+    const axiosInstanceMock = {
+      get: jest.fn().mockResolvedValueOnce({ data: data }),
+    } as unknown as AxiosInstance;
+    const axiosCreateMock = axios.create as jest.MockedFunction<
+      typeof axios.create
+    >;
+    axiosCreateMock.mockReturnValueOnce(axiosInstanceMock);
+    const result = await throttledGetDataFromApi(postsUrl);
+
+    await Promise.resolve();
+    jest.runAllTimers();
+
+    expect(result).toEqual(data);
   });
 });
